ea09f94c38e020f4a10c6b50a8c0b3f4c7256b96
Victor <78874691+victor-teles@users.noreply.github.com>
2024-01-12 20:05:08 -0300

    fix: execution order

--- src/main/kotlin/com/github/biomejs/intellijbiome/actions/ApplySafeFixesOnSaveAction.kt
+++ src/main/kotlin/com/github/biomejs/intellijbiome/actions/ApplySafeFixesOnSaveAction.kt
@@ -4,21 +4,34 @@ import com.github.biomejs.intellijbiome.BiomeBundle
 import com.github.biomejs.intellijbiome.BiomeStdinRunner
 import com.github.biomejs.intellijbiome.settings.BiomeSettings
 import com.intellij.ide.actionsOnSave.impl.ActionsOnSaveFileDocumentManagerListener
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.progress.util.ProgressIndicatorUtils
 import com.intellij.openapi.project.Project
+import java.util.concurrent.CompletableFuture
 
 
 class ApplySafeFixesOnSaveAction : ActionsOnSaveFileDocumentManagerListener.ActionOnSave() {
+
     override fun isEnabledForProject(project: Project): Boolean =
         BiomeSettings.getInstance(project).applySafeFixesOnSave
 
     override fun processDocuments(project: Project, documents: Array<Document?>) {
         val runner = BiomeStdinRunner(project)
+        val onSaveHelper = OnSaveHelper()
+        val future = CompletableFuture<Void>()
 
-        OnSaveHelper().formatDocuments(
+        onSaveHelper.formatDocuments(
             project,
             documents.filterNotNull().toList(),
             BiomeBundle.message("biome.apply.safe.fix.with.biome")
-        ) { request -> runner.applySafeFixes(request) }
+        ) { request ->
+            val response = runner.applySafeFixes(request)
+            future.complete(null)
+
+            response
+        }
+
+        ProgressIndicatorUtils.awaitWithCheckCanceled(future)
     }
 }
