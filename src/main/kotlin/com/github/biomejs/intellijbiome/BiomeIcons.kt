c23e69c9b939f698c4a5780233081e8245b95ce1
JohannesIBK <mail@greuter.dev>
2024-05-19 12:17:46 +0200

    Update widgets  (#59)
    * remove taskbar widget
    
    * update lsp widget entry
    
    - add the biome icon
    - add button to the settings page

---
+++ src/main/kotlin/com/github/biomejs/intellijbiome/BiomeIcons.kt
@@ -0,0 +1,8 @@
+package com.github.biomejs.intellijbiome
+
+import com.intellij.openapi.util.IconLoader
+
+object BiomeIcons {
+    @JvmField
+    val BiomeIcon = IconLoader.getIcon("/icons/pluginIcon.svg", javaClass)
+}
