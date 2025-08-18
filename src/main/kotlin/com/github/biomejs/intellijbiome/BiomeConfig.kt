dbe44926ef42d2621e12d3ec4472b455364a5f84
Naoki Ikeguchi <me@s6n.jp>
2025-08-18 14:55:38 +0900

    fix: support deserialising `extends` as a list (#214)

--- src/main/kotlin/com/github/biomejs/intellijbiome/BiomeConfig.kt
+++ src/main/kotlin/com/github/biomejs/intellijbiome/BiomeConfig.kt
@@ -3,15 +3,22 @@ package com.github.biomejs.intellijbiome
 import com.intellij.openapi.vfs.VirtualFile
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.*
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonTransformingSerializer
 import kotlinx.serialization.json.decodeFromStream
 
 @Serializable
 data class BiomeConfig(
     val root: Boolean? = null,
-    val extends: String? = null,
+
+    @Serializable(with = ExtendsSerializer::class)
+    val extends: List<String>? = null,
 ) {
-    fun isRootConfig() = root != false && extends != "//"
+    fun isRootConfig() =
+        root != false && extends?.contains("//") != true
 
     companion object {
         @OptIn(ExperimentalSerializationApi::class)
@@ -28,5 +35,10 @@ data class BiomeConfig(
                 null
             }
         }
+    }
+
+    object ExtendsSerializer : JsonTransformingSerializer<List<String>>(ListSerializer(String.serializer())) {
+        override fun transformDeserialize(element: JsonElement): JsonElement =
+            element as? JsonArray ?: JsonArray(listOf(element))
     }
 }
